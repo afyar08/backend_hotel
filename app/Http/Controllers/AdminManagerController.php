2cc7a069944d064e4ac794efbb587599493a8f17
Alya_Angraini <116321705+aruya2707@users.noreply.github.com>
2024-04-01 05:34:41 +0700

    manager registration terhubung

--- app/Http/Controllers/AdminManagerController.php
+++ app/Http/Controllers/AdminManagerController.php
@@ -4,28 +4,93 @@ namespace App\Http\Controllers;
 
 use App\Models\ModelManager;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
 
 class AdminManagerController extends Controller
 {
-    public function create(Request $request)
+    public function create_akun(Request $request)
     {
-        // Validasi request
+        try {
+            // Validasi request
+            $validatedData = $request->validate([
+                'nama' => 'required|string',
+                'email' => 'required|email|unique:managers,email',
+                'password' => 'required|min:8',
+            ]);
+
+            // Buat instance ModelManager baru
+            $passwordEncrypted = Crypt::encryptString($validatedData['password']);
+            $manager = new ModelManager([
+                'nama' => $validatedData['nama'],
+                'email' => $validatedData['email'],
+                'password' => $passwordEncrypted,
+            ]);
+
+            // Simpan data Manager ke dalam database
+            $manager->save();
+
+            return response()->json(['message' => 'Akun Manager berhasil dibuat'], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Gagal membuat akun Manager', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function read_akun()
+    {
+        try {
+            //ambil data
+            $managers = ModelManager::all();
+
+            return response()->json(['managers' => $managers], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Gagal membaca data managers','error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function update_akun(Request $request, $id)
+{
+    try {
+        $manager = ModelManager::findOrFail($id);
         $validatedData = $request->validate([
-            'nama' => 'required|string',
-            'email' => 'required|email|unique:managers,email',
-            'password' => 'required|min:8',
-        ]);
+            'nama' => 'sometimes|required|string',
+            'email' => 'sometimes|required|email|unique:managers,email,' . $manager->id,
+            'password' => 'sometimes|required|min:8',
+        ]); 
 
-        // Buat instance ModelManager baru
-        $manager = new ModelManager([
-            'nama' => $validatedData['nama'],
-            'email' => $validatedData['email'],
-            'password' => bcrypt($validatedData['password']), // Enkripsi password
-        ]);
+        if (isset($validatedData['nama'])) {
+            $manager->nama = $validatedData['nama'];
+        }
 
-        // Simpan data Manager ke dalam database
+        if (isset($validatedData['email'])) {
+            $manager->email = $validatedData['email'];
+        }
+
+        if (isset($validatedData['password'])) {
+            $passwordEncrypted = Crypt::encryptString($validatedData['password']);
+            $manager->password = $passwordEncrypted;
+        }
+
         $manager->save();
+        
+        return response()->json(['message' => 'Akun Manager berhasil diperbaharui'], 200);
 
-        return response()->json(['message' => 'Akun Manager berhasil dibuat'], 201);
+    } catch (\Exception $e) {
+        return response()->json(['message' => 'Gagal memperbaharui akun manager', 'error' => $e->getMessage()], 500);
+    }
+}
+
+
+    public function delete_akun($id)
+    {
+        try {
+            $manager = ModelManager::findOrFail($id);
+            $manager->delete();
+
+            return response()->json(['message' => 'Akun berhasil dihapus'], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Gagal menghapus akun', 'error' => $e->getMessage()], 500);
+        }
     }
 }
